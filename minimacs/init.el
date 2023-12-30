28603aa42c3415820c0fb24e614d9e3c6c51099a
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2023-12-30 02:07:51 +0100

    minimacs: Customize

--- minimacs/init.el
+++ minimacs/init.el
@@ -21,12 +21,21 @@
 (windmove-default-keybindings)
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-echo-area-message "mattiasb")
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil))
 
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (provide 'init)
 ;;; init.el ends here
